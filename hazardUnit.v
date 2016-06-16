b1fc65503484473b409644b63380ad74bfa69246
MrwanBaghdad <mrwanbaghdad76@gmail.com>
2016-06-16 22:48:29 +0200

    finished hazard logic

--- hazardUnit.v
+++ hazardUnit.v
@@ -1,10 +1,24 @@
 module hazardUnit(stallF,StallD,branchD,forwardAD,
-	forwardBD,RsD,RtD,FlushE,
+	forwardBD,forwardAE,forwardBE,RsD,RtD,FlushE,
 	RsE,RtE,WriteRegE,MemtoRegE,
 	RegWriteE,WriteRegM,RegWriteM,
 	RegWriteM,WriteRegW,RegWriteW);
 
-output stallF,StallD,forwardBD,forwardBD,FlushE;
-input RegWriteM,RegWriteE,RegWriteW,branchD;
+output stallF,StallD,forwardBD,forwardBD,FlushE,forwardBE,forwardAE;
+input RegWriteM,RegWriteE,RegWriteW,
+		branchD,MemtoRegE;
 
 input [4:0] RsD,RtD;
+
+if(RsE!=0 && RsE == WriteRegM && RegWriteM)
+begin
+	forwardAE<=2'b10 ;
+end 
+else if (RsE!=0 && RsE== WriteRegW && RegWriteW) begin
+		forwardAE=01;
+		end 
+		else begin
+		forwardAE=2'b00;
+	end
+
+end module
